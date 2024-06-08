ec9869c973c8d81283b729e7d9b1380eff3277fa
Daniel C. Opute <danielcopute@gmail.com>
2024-06-08 13:49:23 +0100

    added my portfolio

---
+++ js/about.js
@@ -0,0 +1,43 @@
+// Change photos slide for my about-me page
+k = 0;
+function changePhotos() {
+	let images = [];
+	// let time = 0;
+    // let time_1 = 12600;
+    // let time_2 = 12600;
+    // prompt('hello')
+
+	//image list
+	images[0] = 'images/about-me/daniel-opute.jpg'
+    images[1] = 'images/about-me/daniel-opute.jpg'
+    images[2] = 'images/about-me/danielcopute.png'
+    // images[3] = 'images/about_me/danielcopute.png'
+    // images[4] = 'images/about_me/danielcopute.png'
+    // images[2] = 'images/home/slide-img/megasoftouch-h.png'
+    // images[3] = 'images/home/slide-img/megasoftouch.png'
+    // images[4] = 'images/home/slide-img/count-down.png'
+    lastImg = 'images/about-me/danielcopute.png'
+
+    let imgOne = document.getElementById('my_photos')
+    let imgTwo = document.getElementById('my_photos_2')
+
+	function changeImg(){
+        if(k < images.length){
+            imgOne.src = images[k];
+            imgTwo.src = images[k];
+            k++;
+            time = 13500
+            setTimeout("changePhotos()", time);
+        }
+        else {
+            time = 20050
+            setTimeout("changePhotos()", time);
+            imgOne.src = lastImg;
+            imgTwo.src = lastImg;
+            k = 0;
+        }
+	}
+    changeImg();
+}
+
+changePhotos()
